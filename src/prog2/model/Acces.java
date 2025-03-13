e678ff13d1a4ea6c2026bcb301d259aa94404b77
Ignacio DM <i.dalmaumiravitlles@gmail.com>
2025-03-13 13:54:27 +0100

    Practica 2 comencem

---
+++ src/prog2/model/Acces.java
@@ -0,0 +1,28 @@
+package prog2.model;
+
+public class Acces {
+    public void afegirAllotjament(Allotjament allotjament) {
+    }
+
+    /**
+     * Canvia l'estat de l'accés a tancat
+     */
+    public void tancarAcces() {
+    }
+
+    /**
+     * Canvia l'estat de l'accés a obert
+     */
+    public void obrirAcces() {
+    }
+
+
+    /**
+     * Retorna si l'accés permet accessibilitat amb cotxe o no.
+     * @return
+     */
+    public boolean isAccessibilitat() {
+        return false;//MODIFICAR
+    }
+}
+
